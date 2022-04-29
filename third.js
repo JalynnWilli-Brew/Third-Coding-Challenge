490a17bf983f9deb26bc8cebfa99f17a43d67eb9
Jalynnwilliams-brewster <jswilliams131314@yahoo.com>
2022-04-29 14:22:18 -0400

    second commit

---
+++ third.js
@@ -0,0 +1,24 @@
+//1
+const myFunction = function (num1, num2) {
+    return num1 + num2;
+};
+
+//Arrow Function
+
+arrowMyFunction = (num1, num2) => num1 + num2
+console.log(arrowMyFunction(10, 5));
+
+
+//2 Convert the following function into a 0ne-line arrow function w/o a return statement.
+const add100 = function (money) {
+    return money + 100;
+};
+
+//One-line arrow
+
+arrowAdd100 = money => (money + 100);
+
+//3
+let countLetters = ('Hello my name is Jalynn', 'a')
+
+//Arrow Function
