b6ba7c4454951723a5819f2e8d8e7e7de2d0361f
Jalynnwilliams-brewster <jswilliams131314@yahoo.com>
2022-04-29 14:57:26 -0400

    fourth commit

--- third.js
+++ third.js
@@ -3,7 +3,7 @@ const myFunction = function (num1, num2) {
     return num1 + num2;
 };
 
-//Arrow Function
+// //Arrow Function
 
 arrowMyFunction = (num1, num2) => num1 + num2
 console.log(arrowMyFunction(10, 5));
@@ -19,6 +19,18 @@ const add100 = function (money) {
 arrowAdd100 = money => (money + 100);
 
 //3
-let countLetters = ('Hello my name is Jalynn', 'a')
+function countLetters(string, a){
+    let count = 0;
 
-//Arrow Function
+    for(let i = 0; i < string.length; i++){
+        if (string.charAt(i) == a){
+            count += 1;
+        }
+    }
+return count;
+}
+
+const string = prompt('Hello my name is Jalynn');
+const letterToCheck = prompt('a')
+const result = countLetters(string, letterToCheck)
+console.log(result);
